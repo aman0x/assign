2fd2251e67c1a5b346f5b1655a3814ff43c0a2f5
Aman Singh Chandel <amanchandel4@gmail.com>
2023-02-24 17:05:20 +0530

    code update

--- ques.py
+++ ques.py
@@ -1,9 +1,9 @@
-from bs4 import BeautifulSoup 
 def assemble_segments(text_fragments): 
     # Step 1: Skip any fragment of size less than 8 
     filtered_fragments = [frag for frag in text_fragments if len(frag) >= 8] 
     segments = [] 
     current_segment = "" 
+    
     # Step 2: Keep assembling a segment from the text fragments until its size is > 128 but not more than 256 
     for fragment in filtered_fragments: 
         if len(current_segment + fragment) <= 256: 
@@ -12,16 +12,20 @@ def assemble_segments(text_fragments):
                 segments.append(current_segment) 
                 current_segment = "" 
         else: 
-        # Add as many full 128-length segments as possible 
+            # Add as many full 128-length segments as possible 
             while len(current_segment) + 128 <= 256: 
                 segments.append(current_segment[:128]) 
                 current_segment = current_segment[128:] 
-                # If there's any remaining text, start a new segment with it 
-            if len(current_segment) > 0: 
-                segments.append(current_segment) 
-                current_segment = fragment 
-            # Step 3: If the initial text fragment is too large, split it into chunks of 128 
+            # # If there's any remaining text, start a new segment with it 
+            
+            #Here there was one logical error - it should apend the remaining test to the segments and reset the current segment to and empty string 
+            segments.append(current_segment) 
+            current_segment = fragment 
+    
+    # Step 3: If the initial text fragment is too large, split it into chunks of 128 
     if len(current_segment) > 0: 
         for i in range(0, len(current_segment), 128): 
             segments.append(current_segment[i:i+128]) 
-    return segments 
+        current_segment = "" 
+    
+    return segments
